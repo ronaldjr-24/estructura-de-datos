21bfd181d5dcfba9258b5163361aa87ca0afea4f
Admin <rj.salazara@uea.edu.ec>
2024-12-08 09:38:43 -0500

    actualizado

--- Tarea2/obj/Debug/net9.0/Tarea2.AssemblyInfo.cs
+++ Tarea2/obj/Debug/net9.0/Tarea2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d94c4a0b3f67cb0d9b8e8ab68cf4c94b0983b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
